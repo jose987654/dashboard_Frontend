a727d3c2256f375987315a19af9ecae060444fad
WASSWA JOSEPH <wasswajose9@gmail.com>
2023-11-23 16:58:50 +0300

    updated dashboard

--- src/layout/MainLayout/LogoSection/index.js
+++ src/layout/MainLayout/LogoSection/index.js
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 // material-ui
-import { ButtonBase } from '@mui/material';
+import { ButtonBase, Typography, Stack } from '@mui/material';
+import GoogleIcon from '@mui/icons-material/Google';
 
 // project imports
 import config from '../../../config';
-import Logo from '../../../ui-component/Logo';
+// import Logo from '../../../ui-component/Logo';
 import { MENU_OPEN } from '../../../store/actions';
 
 // ==============================|| MAIN LOGO ||============================== //
@@ -16,7 +17,13 @@ const LogoSection = () => {
   const dispatch = useDispatch();
   return (
     <ButtonBase disableRipple onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })} component={Link} to={config.defaultPath}>
-      <Logo />
+      {/* <Logo /> */}
+      <Stack direction="row" alignItems="center" spacing={0}>
+        <GoogleIcon sx={{ fontSize: '2rem', color: '#5E35B1' }} />
+        <Typography variant="h3" sx={{ fontWeight: 'bold', fontFamily: 'Roboto, sans-serif', color: '#5E35B1' }}>
+          oogle Reports
+        </Typography>
+      </Stack>
     </ButtonBase>
   );
 };
--- src/menu-items/utilities.js
+++ src/menu-items/utilities.js
@@ -1,12 +1,15 @@
 // assets
-import { IconTypography, IconPalette, IconShadow, IconWindmill } from '@tabler/icons';
+import { IconWindmill, IconMail, IconAd, IconSearch } from '@tabler/icons';
 
 // constant
 const icons = {
-  IconTypography,
-  IconPalette,
-  IconShadow,
-  IconWindmill
+  // IconTypography,
+  // IconPalette,
+  // IconShadow,
+  IconWindmill,
+  IconMail,
+  IconAd,
+  IconSearch
 };
 
 // ==============================|| UTILITIES MENU ITEMS ||============================== //
@@ -18,52 +21,52 @@ const utilities = {
   children: [
     {
       id: 'util-typography',
-      title: 'Typography',
+      title: 'Ads API',
       type: 'item',
       url: '/utils/util-typography',
-      icon: icons.IconTypography,
+      icon: icons.IconAd,
       breadcrumbs: false
     },
     {
       id: 'util-color',
-      title: 'Color',
+      title: 'Search API',
       type: 'item',
       url: '/utils/util-color',
-      icon: icons.IconPalette,
+      icon: icons.IconSearch,
       breadcrumbs: false
     },
     {
       id: 'util-shadow',
-      title: 'Shadow',
+      title: 'Email',
       type: 'item',
       url: '/utils/util-shadow',
-      icon: icons.IconShadow,
+      icon: icons.IconMail,
       breadcrumbs: false
-    },
-    {
-      id: 'icons',
-      title: 'Icons',
-      type: 'collapse',
-      icon: icons.IconWindmill,
-      children: [
-        {
-          id: 'tabler-icons',
-          title: 'Tabler Icons',
-          type: 'item',
-          url: '/icons/tabler-icons',
-          breadcrumbs: false
-        },
-        {
-          id: 'material-icons',
-          title: 'Material Icons',
-          type: 'item',
-          external: true,
-          target: '_blank',
-          url: 'https://mui.com/material-ui/material-icons/',
-          breadcrumbs: false
-        }
-      ]
     }
+    // {
+    //   id: 'icons',
+    //   title: 'Icons',
+    //   type: 'collapse',
+    //   icon: icons.IconWindmill,
+    //   children: [
+    //     {
+    //       id: 'tabler-icons',
+    //       title: 'Tabler Icons',
+    //       type: 'item',
+    //       url: '/icons/tabler-icons',
+    //       breadcrumbs: false
+    //     },
+    //     {
+    //       id: 'material-icons',
+    //       title: 'Material Icons',
+    //       type: 'item',
+    //       external: true,
+    //       target: '_blank',
+    //       url: 'https://mui.com/material-ui/material-icons/',
+    //       breadcrumbs: false
+    //     }
+    //   ]
+    // }
   ]
 };
 
