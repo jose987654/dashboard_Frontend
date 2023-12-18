36377ff8f17139a5951bc705f8928f55474dc7b1
WASSWA JOSEPH <wasswajose9@gmail.com>
2023-12-18 23:58:29 +0300

    updated dashboard

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
-// const ipAddress = '127.0.0.1:5000';
+// const ipAddress = '127.0.0.1:5000'; https://jon.seedr.dev/login
 const ipAddress = '23.88.55.176:5000';
-
-const baseUrl = `https://${ipAddress}`;
+// const baseUrl = `https://${ipAddress}`;
+const baseUrl = `https://jon.seedr.dev:5000`;
 
 import { getEmailList, updateEmail, deleteEmail, deleteAllEmail, addEmailList, delEmailList } from './email';
 import { loginFunction, signupFunction, resetPasswordFunction, logoutFunction, logoutFunctioncustom, statusFunction } from './user';
